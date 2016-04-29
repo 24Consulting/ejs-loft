6a98c18ec985128f222c77e13d69740f86ef6b73
Ilya <iplugin@gmail.com>
2016-04-29 23:54:23 +0300

    Updated eslint rules.

--- example/app.js
+++ example/app.js
@@ -2,16 +2,16 @@ var express = require('express'),
 	engine = require('../'),
 	app = express();
 
-app.use(express.static(__dirname + '/static'));
+app.use(express['static'](`${__dirname}/static`));
 app.engine('ejs', engine);
 
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
-app.get('/',(req,res,next) => res.render('index',{
-	what:'best',
-	who:'me',
-	muppets:['Kermit','Fozzie','Gonzo']
+app.get('/', (req, res, ndext) => res.render('index', {
+	what: 'best',
+	who: 'me',
+	muppets: ['Kermit', 'Fozzie', 'Gonzo']
 }));
 
 app.listen(3000);
